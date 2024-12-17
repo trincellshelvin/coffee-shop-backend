fd8ee99ded82ec4f750a9a9efcf341bcb0d85295
Trincell Shelvin <trincellshelvin@gmail.com>
2024-12-16 22:21:25 -0600

    routes created and setup with models

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
+const productRoutes = require('./routes/products');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use('/products', productRoutes);
+
+const mongoURL = process.env.MONGO_URL;
 
 // Connect to MongoDB
 mongoose
-    .connect('your-mongodb-connection-string-here')
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
         console.log('Connected to MongoDB');
     })
--- models/products.js
+++ models/products.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -11,4 +11,4 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
-export default Product;
+module.exports = Product;
